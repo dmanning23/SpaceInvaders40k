5a2adcf5e54a7750f52edcd84fd2e302b8c2c4e0
DUKA141 <82040050+DUKA141@users.noreply.github.com>
2021-04-06 14:57:08 +0100

    Add files via upload

---
+++ WarhammerNecronInvaders/WarhammerNecronInvaders/Game1.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace WarhammerNecronInvaders
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        SpriteFont info;
+        Texture2D Imperialship;
+        Rectangle Imperialshiprect;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            Imperialshiprect = new Rectangle(0, 100, 100, 100);
+
+
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Imperialship = Content.Load<Texture2D>("Imperialship");
+
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            Imperialshiprect.X++;
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
